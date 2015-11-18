7711a0194123ccada71172476d30a0bb0bc6a6e3
Truong Hoang DUng <checkraiser11@gmail.com>
2015-11-18 16:36:49 +0700

    Add profiler

--- railsapp/app/controllers/application_controller.rb
+++ railsapp/app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action do
+    if current_user
+      Rack::MiniProfiler.authorize_request
+    end
+  end
 end
---
+++ railsapp/config/initializers/rack_profiler.rb
@@ -0,0 +1,12 @@
+if Rails.env == 'development'
+  require 'rack-mini-profiler'
+
+  # initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+end
+Rack::MiniProfiler.config.disable_caching = false # defaults to true
+if Rails.env.production?
+  uri = URI.parse(ENV["REDIS_URL"])
+  Rack::MiniProfiler.config.storage_options = { :host => uri.host, :port => uri.port, :password => uri.password }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+end
